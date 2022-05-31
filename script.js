dc34b5ffbb12e1c3a7ced160ff7220656590489c
JamesDa1 <104003738+JamesDa1@users.noreply.github.com>
2022-05-31 14:13:16 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,30 @@
+const fetchImgBtn = document.querySelector("#fetchImgBtn");
+const checkBlur = document.querySelector("#blur");
+
+fetchImgBtn.addEventListener("click", () => {
+  let width = document.querySelector("#width").value;
+  let height = document.querySelector("#height").value;
+  console.log(width, height);
+  // checks for black/white option
+  let grayScaleActive = document.querySelector("#grayScale").checked
+    ? "?grayscale"
+    : "";
+
+  let blurCode = "";
+  if (checkBlur.checked) {
+    let blurStrength = document.querySelector("#blurIntensity").value;
+    if (grayScaleActive) {
+      blurCode = `&blur=${blurStrength}`;
+    } else {
+      blurCode = `?blur=${blurStrength}`;
+    }
+  }
+
+  let link = `https://picsum.photos/${width}/${height}${grayScaleActive}${blurCode}`;
+
+  fetch(link).then((response) => {
+    let img = document.createElement("img");
+    img.src = response.url;
+    document.querySelector("#images").appendChild(img);
+  });
+});
